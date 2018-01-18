cf727d7b9a1694ad58f31828fc6854f74eb0a6b9
Brittni Watkins <blwatkins@smu.edu>
2018-01-18 13:13:10 -0600

    Complte institution script. Edit work written script to improve readability.

--- DatabaseBuilder/institution.sqlite.sql
+++ DatabaseBuilder/institution.sqlite.sql
@@ -3,5 +3,5 @@
 -- Script for creating the institution table
 
 CREATE TABLE Institution (
-
+    type VARCHAR(40) PRIMARY KEY NOT NULL
 );
--- DatabaseBuilder/work_written.sqlite.sql
+++ DatabaseBuilder/work_written.sqlite.sql
@@ -9,6 +9,6 @@ CREATE TABLE WorkWritten (
     author_lastName VARCHAR(40) NOT NULL,
     PRIMARY KEY (work_ISBN, author_firstName, author_middleName, author_lastName),
     FOREIGN KEY (work_ISBN) REFERENCES Work(ISBN),
-    FOREIGN KEY (author_firstName, author_middleName, author_lastName) REFERENCES
-        Author(firstName, middleName, lastName)
+    FOREIGN KEY (author_firstName, author_middleName, author_lastName)
+    	REFERENCES Author(firstName, middleName, lastName)
 );
