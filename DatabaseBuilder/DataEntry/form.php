e2f83fa68b7ec2063df58cf114b465d105d115d0
Brittni Watkins <blwatkins@smu.edu>
2018-01-18 13:52:46 -0600

    Add initial file for form script.

---
+++ DatabaseBuilder/DataEntry/form.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<!-- Banned Books -->
+<!-- Data Enry Form -->
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title> Banned Books - Data Entry Form </title>
+    <head>
+    <body>
+        
+    </body>
+</html>
