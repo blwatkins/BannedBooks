e0f6d068c86bdc6fbc1a45fecf53e6d07158e2b1
Brittni Watkins <blwatkins@smu.edu>
2018-01-18 13:21:51 -0600

    Complte script for creating challenge table.

--- DatabaseBuilder/challenge.sqlite.sql
+++ DatabaseBuilder/challenge.sqlite.sql
@@ -3,5 +3,14 @@
 -- Script for creating the challenge table
 
 CREATE TABLE Challenge (
-
+    work_ISBN INT(13) NOT NULL,
+    state CHAR(2) NOT NULL,
+    institution VARCHAR(40) NOT NULL,
+    year INT(4) NOT NULL,
+    count INT NOT NULL,
+    source_url TEXT,
+    PRIMARY KEY (work_ISBN, state, institution, year),
+    FOREIGN KEY (work_ISBN) REFERENCES Work(ISBN),
+    FOREIGN KEY (state) REFERENCES State(abbreviation),
+    FOREIGN KEY (institution) REFERENCES Institution(type)
 );
