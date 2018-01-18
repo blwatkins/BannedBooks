e782d313916ef2f136b7a62ce6125757bed4993c
Brittni Watkins <blwatkins@smu.edu>
2018-01-18 12:41:32 -0600

    Add initial file for creating work table. Add work table read statement to create tables file.

--- DatabaseBuilder/create_tables.sqlite.sql
+++ DatabaseBuilder/create_tables.sqlite.sql
@@ -3,7 +3,8 @@
 -- Script for creating all the tables needed for the Banned and Challenged Books database
 
 PRAGMA foreign_keys = ON;
-.mode col;
-.headers on;
+.mode col
+.headers on
 
-.read author.sqlite.sql;
+.read author.sqlite.sql
+.read work.sqlite.sql
---
+++ DatabaseBuilder/work.sqlite.sql
@@ -0,0 +1,9 @@
+-- Banned and Challenged Books
+-- Works Table
+-- Script for creating the work table
+-- A work represents any literary entity that has been published at least once as a book or ebook
+-- Works may be published any number of times by various publishing houses in various formats
+
+CREATE TABLE Work (
+
+);
